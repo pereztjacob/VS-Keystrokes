b6f5d2d8540130d8e50c7ec6b59e8c91da4aec4e
pereztjacob <pereztjacob@gmail.com>
2017-11-13 14:35:10 -0800

    renders first three cards

--- learn.js
+++ learn.js
@@ -4,7 +4,11 @@ function ShortCut(keys, description, gifURL){
     this.gifURL = gifURL;
 }
 
-const save = new ShortCut(['ctrl', 'S', '#'], 'saves file locally');
+const save = new ShortCut(['ctrl', 'S'], 'saves file locally', '#');
+const search = new ShortCut(['ctrl', 'f'], 'searches for term within a page', '#');
+const lineMove = new ShortCut(['ctrl', 'up'], 'moves line up or down within your code', '#');
+
+objArray = [save, search, lineMove];
 
 ShortCut.prototype.renderCards = function(keys, description){
     const game = document.getElementById('game');
@@ -19,5 +23,5 @@ ShortCut.prototype.renderCards = function(keys, description){
 }
 
 for(let i = 0; i < 3; i++){
-    ShortCut.prototype.renderCards(save.keys[0] + ' + ' + save.keys[1], save.description);
+    ShortCut.prototype.renderCards(objArray[i].keys[0] + ' + ' + objArray[i].keys[1], objArray[i].description);
 }
